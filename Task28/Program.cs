732b5f5c254c10e12bb03739814c2934d9125531
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2022-11-12 17:41:36 +0700

    add Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число А и выдает произведение чисел от 1 до А
+
+void GetSumNums(int number)
+{
+    int sum = 1;
+    for (int i = 1; i <= Math.Abs(number); i++)
+    {
+        sum *= i;
+    }
+    Console.WriteLine($"Сумма цифр от 1 до введеного числа А = {sum}");
+}
+Console.Clear();
+Console.Write("Ввведите число A: ");
+GetSumNums(Convert.ToInt32(Console.ReadLine()));
