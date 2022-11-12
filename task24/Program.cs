a554b7b698142b09c4c2746b5dd9cb702333a812
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2022-11-12 17:12:57 +0700

    add seminar task24 final2

--- task24/Program.cs
+++ task24/Program.cs
@@ -2,12 +2,12 @@
 
 void GetSumNums(int number)
 {
-int sum = 0;
-for (int i = 1; i <= Math.Abs(number); i++)
-{
-    sum += i;
-}
-Console.WriteLine($"Сумма цифр от 1 до введеного числа А = {sum}");
+    int sum = 0;
+    for (int i = 1; i <= Math.Abs(number); i++)
+    {
+        sum += i;
+    }
+    Console.WriteLine($"Сумма цифр от 1 до введеного числа А = {sum}");
 }
 Console.Clear();
 //Console.Write("Ввведите число A: ");
