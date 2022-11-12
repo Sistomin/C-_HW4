8f115cc27420ec0d4f9919b1b2856a564c1c3249
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2022-11-12 16:38:01 +0700

    add seminar task24

---
+++ task24/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число А и выдает сумму чисел от 1 до А
+
+void GetSumNums(int number)
+{
+int sum = 0;
+for (int i = 1; i <= Math.Abs(number); i++)
+{
+    sum += i;
+}
+Console.WriteLine($"Сумма цифр от 1 жо введеного числа: {sum}");
+}
+Console.Clear();
+GetSumNums(5);
+GetSumNums(50);
+GetSumNums(25);
