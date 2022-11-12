0c3c42c23822781b9368dcdeb3099e6e269c37cf
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2022-11-12 17:28:53 +0700

    add TaskSeminar2

---
+++ TaskSeminar2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, коорая принимает на вход число N и выдает количество цифр в числе.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int sum = GetCountNums(num);
+Console.WriteLine($"Количество цифр = {sum}");
+
+int GetCountNums(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        count++;
+        number /= 10;
+    }
+    return count;
+}
